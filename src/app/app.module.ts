7afe412475b2bb26b113fdad79f1f2fcc49dc040
Matteoufficio <matteo.pipitone93@gmail.com>
2017-12-06 14:55:15 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,37 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { RouterModule, Routes } from '@angular/router';
+import { MatButtonModule, MatCardModule, MatInputModule, MatSnackBarModule, MatToolbarModule } from '@angular/material';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
 
+const myRoots: Routes = [
+
+  {path: 'register',component: RegistrationComponent},
+  {path: '',component: HomeComponent}
+
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        RegistrationComponent,
+        HomeComponent,
+        NavComponent
+    ],
+    imports: [
+        BrowserModule,
+        //AppRoutingModule,
+        RouterModule.forRoot(myRoots),
+        MatButtonModule, MatCardModule, MatInputModule, MatSnackBarModule, MatToolbarModule
+
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
+
+
+
 export class AppModule { }
